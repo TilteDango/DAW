6d34d17dcc467654c73d65af7663f551343cc793
TilteDango <120929304+TilteDango@users.noreply.github.com>
2023-02-12 21:36:52 +0100

    Add files via upload

---
+++ C#/Examenes/Examen Programación/Examen Segundo Trimestre/Examen_Ej3/Examen_Ej3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Examen_Ej3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvierteMayusculas("texto.txt", "copia.txt");
+        }
+
+        static void InvierteMayusculas (string fichero, string copia)
+        {
+            string aux="", aux2 ="";
+            StreamWriter sw = new StreamWriter(copia);
+            StreamReader sr = new StreamReader(fichero);
+
+            while (!sr.EndOfStream)
+            {
+                aux = aux + sr.ReadLine();
+            }
+
+            for (int i = 0; i < aux.Length; i++)
+            {
+                if (char.IsLetter(aux[i]))
+                {
+                    if (char.IsUpper(aux[i]))
+                    {
+                        aux2 = aux2 + char.ToLower(aux[i]);
+                    }
+                    else
+                    {
+                        aux2 = aux2 + char.ToUpper(aux[i]);
+                    }
+                }
+                else
+                {
+                    aux2 = aux2 + aux[i];
+                }
+
+                
+            }
+            sw.WriteLine(aux2);
+
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
---
+++ C#/Examenes/Examen Programación/Examen Segundo Trimestre/Examen_Ej4/Examen_Ej4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Examen_Ej4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableroAjedrez = { { 0, 0, 0, 0, 0, 1, 0, 0 },
+                          { 0, 0, 0, 1, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 0, 0, 1, 0 },
+                          { 1, 0, 0, 0, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 0, 0, 0, 1 },
+                          { 0, 1, 0, 0, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 0, 1, 0, 0 },
+                          { 0, 0, 1, 0, 0, 0, 0, 0 } };
+            Console.WriteLine(CompruebaDamas(tableroAjedrez));
+        }
+
+        static bool CompruebaDamas (int[,] tablero)
+        {
+            int cont = 1;
+            bool ataca = false;
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (tablero[x,y] != 0)
+                    {
+                        //Calcula horizontal
+                        for (int j = 0; j < y; j++)
+                        {
+                            if (tablero[x,j] != 0)
+                            {
+                                ataca = true;
+                            }
+                        }
+                        for (int j = y +1; j < 8; j++)
+                        {
+                            if (tablero[x,j] != 0)
+                            {
+                                ataca = true;
+                            }
+                        }
+                        //Calcula vertical
+                        for (int j = 0; j < x; j++)
+                        {
+                            if (tablero[j, y] != 0)
+                            {
+                                ataca = true;
+                            }
+                             
+                        }
+                        for (int j = x +1; j < 8; j++)
+                        {
+                            if (tablero[j,y] != 0)
+                            {
+                                ataca = true;
+                            }
+                        }
+                        //Calcular diagonal
+                        cont = 1;
+                        while (x + cont < 8 && y + cont < 8)
+                        {
+                            if (tablero[x+cont, y+cont] != 0)
+                            {
+                                ataca = true;
+                            }
+                            cont++;
+                        }
+                        cont = 1;
+
+                        while (x + cont < 8 && y - cont > 0)
+                        {
+                            if (tablero[x+cont, y-cont] != 0)
+                            {
+                                ataca = true;
+                            }
+                            cont++;
+                        }
+                        cont = 1;
+                        while (x - cont > 0 && y - cont > 0)
+                        {
+                            if (tablero[x-cont,y-cont] != 0)
+                            {
+                                ataca = true;
+                            }
+                            cont++;
+                        }
+                        cont = 1;
+                        while (x - cont > 0 && y + cont < 8)
+                        {
+                            if (tablero[x-cont,y+cont] != 0)
+                            {
+                                ataca = true;
+                            }
+                            cont++;
+                        }
+
+                    }
+
+                    
+                }
+            }
+            return ataca;
+        }
+
+        
+    }
+}
