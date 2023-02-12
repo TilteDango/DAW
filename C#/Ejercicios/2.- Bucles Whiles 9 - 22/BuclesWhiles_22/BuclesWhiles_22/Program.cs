a264ed5ac3aa9f7d8d8a6c5ef5c097d12bd2ba0f
TilteDango <120929304+TilteDango@users.noreply.github.com>
2023-02-12 21:32:11 +0100

    Add files via upload

---
+++ C#/Ejercicios/2.- Bucles Whiles 9 - 22/BuclesWhiles_22/BuclesWhiles_22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuclesWhiles_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, fact;
+            fact = 1;
+            Console.WriteLine("Dime un número que quiera el factorial");
+            n = int.Parse(Console.ReadLine());
+           
+            i = n;
+            while (i >= 1)
+            {
+                fact = fact * i;
+                i--;
+            }
+
+            Console.WriteLine("El número sería " + fact);
+        }
+    }
+}
---
+++ C#/Ejercicios/2.- Bucles Whiles 9 - 22/BulcesWhiles_14/BulcesWhiles_14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BulcesWhiles_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b;
+
+            Console.WriteLine("Dime dos números de menos a mayor");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dime el otro número");
+            b = int.Parse(Console.ReadLine());
+
+            while (a > b)
+            {
+                Console.WriteLine("Así no es, dime otra vez los números");
+                 a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Dime el otro número");
+                b = int.Parse(Console.ReadLine());
+            }
+
+            n = a;
+
+            while (a <= b)
+            {
+                Console.WriteLine(a);
+
+                a++;
+            }
+        }
+    }
+}
