7784402a16faab3efb5a5a1ec7f68740c5651236
TilteDango <120929304+TilteDango@users.noreply.github.com>
2023-02-12 21:23:54 +0100

    Add files via upload

---
+++ C#/Ejercicios/1.- Estructurada 1 - 8/Estructurada_01/Estructurada_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Estructurada_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, resultado;
+
+            Console.WriteLine("Dime el número A");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dime el número B");
+            b = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                resultado = (a - b);
+            }
+            else
+            {
+                resultado = (b - a);
+            }
+
+            Console.WriteLine("El resultado es " + resultado);
+        }
+    }
+}
---
+++ C#/Ejercicios/1.- Estructurada 1 - 8/Estructurada_03/Estructurada_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Estructurada_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, resultado;
+            string moneda;
+            Console.WriteLine("Dime el número de la moneda");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dime a que quieres convertirlo (peseta/euro)");
+            moneda = Console.ReadLine();
+
+            if (moneda == "peseta")
+            {
+                resultado = (n1 * 166.386);
+
+                Console.WriteLine("Serían " + resultado + " pesetas");
+            }
+            else
+            {
+                resultado = (n1 / 166.386);
+
+                Console.WriteLine("Serían " + resultado + " euros");
+            }
+        }
+    }
+}
