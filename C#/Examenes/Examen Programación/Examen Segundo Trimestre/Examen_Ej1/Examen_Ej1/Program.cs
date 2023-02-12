1170e91040c71b7be22076b2a1dd6283eea56912
TilteDango <120929304+TilteDango@users.noreply.github.com>
2023-02-12 21:35:06 +0100

    Add files via upload

---
+++ C#/Examenes/Examen Programación/Examen Segundo Trimestre/Examen_Ej1/Examen_Ej1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Examen_Ej1
+{
+    class Program
+    {
+        public class Ips
+        {
+            public int p1;
+            public int p2;
+            public int p3;
+        }
+        static void Main(string[] args)
+        {
+            GeneraServidorDHCP("GeneraServidorDHCP.txt", 1000);
+        }
+
+        static void GeneraServidorDHCP (string fichero, int n)
+        {
+            int[] ips = new int[3];
+
+            bool esta = false;
+            List<Ips> usadas = new List<Ips>();
+
+            StreamWriter sw = new StreamWriter(fichero);
+            Random r = new Random();
+            sw.WriteLine("subnet 10.0.0.0 netmask 255.0.0.0 {");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < ips.Length; k++)
+                {
+                    ips[k] = r.Next(0, 255);
+                }
+
+                for (int k = 0; k < usadas.Count; k++)
+                {
+                    if (ips[0] == usadas[k].p1 && ips[1] == usadas[k].p2 && ips[0] == usadas[k].p3)
+                    {
+                        esta = true;
+                    }
+                }
+
+                if (!esta)
+                {
+                    sw.WriteLine("   range "+ 10 + "." + ips[0] + "." + ips[1] + "." + ips[2] + ";");
+                    Ips us = new Ips();
+                    us.p1 = ips[0];
+                    us.p2 = ips[1];
+                    us.p3 = ips[2];
+                    usadas.Add(us);
+                    esta = false;
+                }
+                
+                
+            }
+
+
+            sw.WriteLine("}");
+            sw.Close();
+           
+        }
+    }
+}
---
+++ C#/Examenes/Examen Programación/Examen Segundo Trimestre/Examen_Ej2/Examen_Ej2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Examen_Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompruebaCoordenadas("tarjeta5x5.cdn"));
+        }
+
+        static bool CompruebaCoordenadas (string fichero)
+        {
+            Random r = new Random();
+            
+            FileStream fs = new FileStream(fichero, FileMode.Open); 
+            BinaryReader br = new BinaryReader(fs);
+            string[,] tarjeta = new string[br.ReadInt32(), br.ReadInt32()];
+            int col = r.Next(0, tarjeta.GetLength(1)), row = r.Next(0,tarjeta.GetLength(0));
+            string palabra;
+            bool final;
+            for (int x = 0; x < tarjeta.GetLength(0); x++)
+            {
+                for (int y = 0; y < tarjeta.GetLength(1); y++)
+                {
+                    tarjeta[x,y] = br.ReadString();
+                }
+            }
+
+
+            Console.Write("Necesito que me introduzcas la cadena de " + (row+1) + "," + (col+1) + ": ");
+            palabra = Console.ReadLine();
+           
+            if (palabra == tarjeta[row, col]) 
+            {
+                final = true;
+            }
+            else
+            {
+                final =  false;
+            }
+
+            br.Close();
+            fs.Close();
+
+            return final;
+  
+        }
+    }
+}
